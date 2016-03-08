87a10992cd408ecebc9adec48d9d5cfb44e134ab
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-03-08 13:44:17 +0700

    edit class method sudah ada sksnya

--- src/Siam.java
+++ src/Siam.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,26 +14,33 @@
 public class Siam {
 
 public String nama;
-public int nim;
+public String nim;
 public String jurusan;
 public Double ipk;
 public String matkul;
 public int sks;
+public int krs;
+
+
+Scanner in = new Scanner(System.in);
 
 public void setNama(String nm){ 
     nama = nm;
 }
-public void setNim(int im){
+public void setNim(String im){
     nim = im;
 }
 public void setJurusan(String ju){
     jurusan = ju ;
 }
-public void setIpk(Double ip){
-    ipk = ip;
-}
 
-public void setSks(int sk){
+public void displayMessage(){
+        System.out.println("Nama    = "+nama);
+        System.out.println("Nim     = "+nim);
+        System.out.println("Jurusan = "+jurusan);
+        System.out.println("ipk     = "+ipk);
+    }
+public void jumlahSks(){
     if (ipk > 3.5)
         System.out.println("24-22 sks");
     else if (ipk < 3.5)
@@ -42,10 +52,21 @@ public void setSks(int sk){
     else
         System.out.println("12 sks");
 }
-public void displayMessage(){
-        System.out.println("Nama    = "+nama);
-        System.out.println("Nim     = "+nim);
-        System.out.println("Jurusan = "+jurusan);
-        System.out.println("ipk     = "+ipk);
+public void krs(){
+    System.out.println("Menu KRS");
+    System.out.println("1.Banyak Sks yang bisa diambil");
+    System.out.println("2.Pilih Matkul");
+    System.out.println("3.Cetak Krs");
+    System.out.println("4.Keluar");
+    System.out.println("Masukkan pilhan anda = ");
+    int pilih = in.nextInt();
+    switch(pilih){
+        case 1 : System.out.println("Banyak Sks yang bisa diambil");
+        case 2 : System.out.println("Pilih Matkul");
+                 
+                
     }
+
+}
+    
 }
