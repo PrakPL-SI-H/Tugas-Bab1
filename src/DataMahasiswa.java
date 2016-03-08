0063ed4f2e7436e4c35a1e6994554076bd9653eb
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-03-08 11:20:29 +0700

    class untuk mahasiswa pertama,belum ada IPK atau sks

---
+++ src/DataMahasiswa.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soal_jawaban_laporan;
+
+/**
+ *
+ * @author TEGAR
+ */
+public class DataMahasiswa {
+   public String nama;
+   public String NIM;
+   public double IP;
+   public String jurusan;
+   
+
+   
+  public void displaymessage(){
+            System.out.println("nama anda    : "+nama);
+            System.out.println("NIM anda     : "+NIM);
+            System.out.println("IP anda      : "+IP);
+            System.out.println("jurusan anda : "+jurusan);
+            
+  }
+  } 
+   
+   
+
