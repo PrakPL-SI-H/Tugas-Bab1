0f6bf3a7452dd1d1555f08ea22edb842fbab5b46
rodhirf <rodhirobb@gmail.com>
2016-03-08 11:20:54 +0700

    membuat class tugas1
    deklarasi atribut-atribut yang dibutuhkan

--- src/LatihanBab1.java
+++ src/LatihanBab1.java
@@ -1,17 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
-public class LatihanBab1 {
-    public static void main(String[] args) {
-        System.out.println("tess");
-        
-        
+package prakPL;
+
+import java.util.Scanner;
+
+public class tugas1 {
+
+    String nama, jurusan, matkul, NIM;
+    int sks, kode;
+    double IP;
+
+    public void setNama(String a) {
+        nama = a;
     }
-    
-}
+
+    public void setNIM(String b) {
+        NIM = b;
+    }
+
+    public void setJurusan(String c) {
+        jurusan = c;
+    }
+
+    public void setMatkul(String e, int f) {
+        matkul = e;
+        kode = f;
+    }
