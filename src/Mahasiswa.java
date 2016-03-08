13dff444d83cf3c081eae8350646a9def8ff32a2
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-03-08 13:50:37 +0700

    edit class ip , menambahkan daftar mata kuliah dan tampilan akhir siam

--- src/Mahasiswa.java
+++ src/Mahasiswa.java
@@ -8,20 +8,38 @@ public class Mahasiswa {
     public double ip;
     
     
-    public void ip(double s){
-        if(4.0 > ip){
+    public double ipk(){
+        if(4.0 >= ip && ip >= 3.0){
             System.out.println("Anda bisa mengambil maksimal 24 SKS");
     }
-        else if(2.99 > ip && ip > 2.5){
+        else if(2.99 >= ip && ip >= 2.5){
             System.out.println("Anda bisa mengambil maksimal 21 SKS");
     }
-        else if(2.00 > ip && ip < 2.49 ){
+        else if(2.00 >= ip && ip <= 2.49 ){
             System.out.println("Anda bisa mengambil maksimal 18 SKS");
         }
-        else if(1.50 > ip && ip < 1.99){
+        else if(1.50 >= ip && ip <= 1.99){
             System.out.println("Anda bisa mengambil maksimal 15 SKS");
         }
-        else if(1.50 < ip)
+        else if(1.50 < ip){
             System.out.println("Anda bisa mengambil maksimal 12 SKS");
-}
+    }
+        System.out.print("Dengan IP ");
+        return ip;
+    }
+    
+    public void mataKuliah(){
+        System.out.println("      Mata Kuliah          | Kode Mata Kuliah |");
+        System.out.println("1. Bahasa Indonesia        |         1        |");
+        System.out.println("2. Sistem Fungsional Bisnis|         2        |");
+        System.out.println("3. Sistem Operasi          |         3        |");
+        System.out.println("4. Pemrograman Lanjut      |         4        |");
+        System.out.println("5. IMK                     |         5        |");
+    }
+    
+    public void tampilkan(){
+        System.out.println("");
+        System.out.println("Mahasiswa bernama "+nama);
+        System.out.println("Dengan NIM "+nim);
+    }
 }
