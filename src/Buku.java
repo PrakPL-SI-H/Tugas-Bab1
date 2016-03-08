569f5646f1da2302e5355dda248810a7f46d50c7
FajarAH <akbaransyah@gmail.com>
2016-03-08 13:06:35 +0700

    Mengerjakan latihan nomor 9

---
+++ src/Buku.java
@@ -0,0 +1,17 @@
+
+public class Buku {
+    int lembar = 50;
+    int kata;
+    int halaman;
+    int hari;
+    public int kata () {
+        return kata = 100 * 50 * 2 * 2;
+    }
+    public int halaman (){
+        return halaman = lembar * 2;
+    }
+    public int hari (){
+        return hari = halaman * 2;
+    }
+    
+}
