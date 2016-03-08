dc8e41f9ed37f08cecdbe5e9e44ecf42cd6bed3e
Nofry <nofryarman@gmail.com>
2016-03-08 13:14:23 -0800

    membuat class SIAM yang berisi attribut dan method2 dari class tersebut yaitu setData, setMatkul, dan setIP

--- src/LatihanBab1.java
+++ src/LatihanBab1.java
@@ -1,13 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
+import java.util.Scanner;
+class SIAM{
+    String nama, nim, jurusan, kodeMatkul, matkul;;
+    double IP, SKS;
+    
+    public void setData( String Nama, String Nim, String Jurusan ){
+        Nama = nama;
+        Nim = nim;
+        Jurusan = jurusan;
+    }
+    public void setMatkul ( String Kode, String Matkul){
+        Kode = kodeMatkul;
+        Matkul = matkul;
+    }
+    public void setIP ( double IndexPres, double jmlSKS ){
+        IndexPres = IP;
+        jmlSKS = SKS;
+        if ( IndexPres >=3.0 && jmlSKS >= 22  ){
+            System.out.println("Valid");
+        }
+        else if ( IndexPres >=2.5 && IndexPres <3.0 && jmlSKS <= 19  ){
+            System.out.println("Valid");
+        }
+        else if ( IndexPres >=2.0 && IndexPres <2.5 && jmlSKS <= 16  ){
+            System.out.println("Valid");
+        }
+        else if ( IndexPres >=1.5 && IndexPres <2.0 && jmlSKS <= 12  ){
+            System.out.println("Valid");
+        }
+        else if ( IndexPres <1.5 && jmlSKS <12  ){
+            System.out.println("Valid");
+        }
+        else {
+            System.out.println("Tidak Valid");
+        }
+    }
+}
 public class LatihanBab1 {
+   
     public static void main(String[] args) {
         System.out.println("tess");
         
