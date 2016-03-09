4629de8cd3670b2e7cdc2d920aa4c328c3aacb70
Rasyid <mrasyid1@yahoo.com>
2016-03-08 20:17:35 -0800

    Pembuatan Main class untuk SIAM Mahasiswa 1,2,& 3.

--- src/LatihanBab1.java
+++ src/LatihanBab1.java
@@ -1,27 +1,18 @@
-import java.util.Scanner;
-
-
-
-public class LatihanBab1 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
-        
-        
-    }
+import java.util.Scanner;        
     //class yang akan digunakan dalam main methd
     class SIAM{
     //atribute dari kelas
-    String nama,nim,kodematkul,matkul;
+    String nama,nim,jurusan, kode,matkul;
     double IP,SKS;
     //method untuk inisialisasi paramater dengan atribute
-    public void NrBioD (String nama, String nim){
+    public void NrBioD (String nama, String nim, String Jurusan){
         this.nama = nama;
         this.nim = nim;     
+        this.jurusan = Jurusan;
     }
     //method untuk inisialisasi paramater dengan atribute
     public void NrMatkul (String Kode, String Matkul){
-        this.kodematkul = Kode;
+        this.kode = Kode;
         this.matkul = Matkul;
     }
     //method untuk inisialisasi paramater dengan atribute 
@@ -43,7 +34,40 @@ public class LatihanBab1 {
         else {
             System.out.println("Tidak Valid");
     }
-    
             }
+    }
+
+public class LatihanBab1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //inisialisasi obect Profile1 dalam class SIAM
+        SIAM Profile1 = new SIAM ();
+        //sistem perulangan untuk memasukkan 3 data mahasiswa
+        for(int i=0; i<3; i++){        
+        //Syntax untuk memasukkan data diri
+        System.out.println("--====Selamat Datang Di SIAM(Sistem Informasi Akdamik Mahasiswa)====--");
+        System.out.print("Masukkan Nama              : ");
+        Profile1.nama=in.next();
+        System.out.print("Masukkan NIM               : ");
+        Profile1.nim = in.next();
+        System.out.print("Masukka Jurusan            : ");
+        Profile1.jurusan = in.next();         
+        System.out.println();
+        System.out.print("Masukkan IP Semester lalu : ");
+        Profile1.IP = in.nextDouble();
+        //Syntax untuk memasukkan informasi Mata Kuliah
+        System.out.print("Masukkan Jumlah Mata Kuliah: ");
+        int Matkul = in.nextInt();
+        for (int n = 0 ; n<Matkul ; n++){      
+        System.out.print("Masukkan Mata Kuliah       : ");
+        Profile1.matkul = in.next();
+        System.out.print("Masukkan Kode              : ");
+        Profile1.kode = in.next();  
+        }
+        System.out.print("Masukkan SKS                : ");    
+        Profile1.SKS = in.nextDouble();     
+        Profile1.NrIP(Profile1.IP, Profile1.SKS);
+        System.out.println();  
+        }
     }
 }
