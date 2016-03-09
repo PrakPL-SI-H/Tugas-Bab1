0d4e1a40b341ce4eb6d842366ecabf2297e89f6c
155150401111138 <rahmatadrian749@gmail.com>
2016-03-09 13:28:26 +0700

    memperbaiki source code siam1

--- src/siam1.java
+++ src/siam1.java
@@ -1,4 +1,3 @@
-
 package OOPpraktikumBAB1;
 import java.util.Scanner;
 public class siam1 {
@@ -6,64 +5,64 @@ public class siam1 {
         Scanner in = new Scanner (System.in);
          
          //orang pertama
-         //siam2 sam = new siam2();
+         siam2 sam = new siam2();
          
-         //System.out.println("=<SISTEM INFORMASI AKADEMIK MAHASISWA>=");
-         //System.out.print("Nama Mahasiswa : ");
-         //sam.nama = in.nextLine();
-         //System.out.print("NIM : ");
-         //sam.nim = in.nextLine();
-         //System.out.print("Jurusan : ");
-         //sam.jur = in.nextLine();
-         //sam.aturData(sam.nama, sam.nim, sam.jur);
+         System.out.println("=<SISTEM INFORMASI AKADEMIK MAHASISWA>=");
+         System.out.print("Nama Mahasiswa : ");
+         sam.nama = in.nextLine();
+         System.out.print("NIM : ");
+         sam.nim = in.nextInt();
+         System.out.print("Jurusan : ");
+         sam.jur = in.nextLine();
+         sam.aturData(sam.nama, sam.nim, sam.jur);
          
-         //System.out.println("===============================");
-         //System.out.print("Jumlah Mata Kuliah : ");
-         //sam.jum = in.nextInt();
+         System.out.println("===============================");
+         System.out.print("Jumlah Mata Kuliah : ");
+         sam.jum = in.nextInt();
          
-         //for (int i=0 ; i<sam.jum ; i++){
-         //    System.out.print("Mata Kuliah : ");
-         //    sam.mk = in.next();
-         //    System.out.print("Kode        : ");
-         //    sam.code = in.nextInt();
-         //}
-         //sam.aturMataKuliah(sam.mk, sam.code);
+         for (int i=0 ; i<sam.jum ; i++){
+             System.out.print("Mata Kuliah : ");
+             sam.mk = in.next();
+             System.out.print("Kode        : ");
+             sam.code = in.nextInt();
+         }
+         sam.aturMataKuliah(sam.mk, sam.code);
          
-         //System.out.print("IP       : ");
-         //sam.ip = in.nextFloat();
-         //System.out.print("SKS      : ");
-         //sam.sks = in.nextInt();
-         //sam.aturIP(sam.ip,sam.sks);
+         System.out.print("IP       : ");
+         sam.ip = in.nextFloat();
+         System.out.print("SKS      : ");
+         sam.sks = in.nextInt();
+         sam.aturIP(sam.ip,sam.sks);
          
          //orang kedua
-         //siam2 sam2 = new siam2();
+         siam2 sam2 = new siam2();
          
-         //System.out.println("=<SISTEM INFORMASI AKADEMIK MAHASISWA");
-         //System.out.print("Nama Mahasiswa : ");
-         //sam2.nama = in.nextLine();
-         //System.out.print("NIM : ");
-         //sam2.nim = in.nextLine();
-         //System.out.print("Jurusan : ");
-         //sam2.jur = in.nextLine();
-         //sam2.aturData(sam2.nama, sam2.nim, sam2.jur);
+         System.out.println("=<SISTEM INFORMASI AKADEMIK MAHASISWA");
+         System.out.print("Nama Mahasiswa : ");
+         sam2.nama = in.nextLine();
+         System.out.print("NIM : ");
+         sam2.nim = in.nextInt();
+         System.out.print("Jurusan : ");
+         sam2.jur = in.next();
+         sam2.aturData(sam2.nama, sam2.nim, sam2.jur);
          
-         //System.out.println("===============================");
-         //System.out.print("Jumlah Mata Kuliah : ");
-         //sam2.jum = in.nextInt();
+         System.out.println("===============================");
+         System.out.print("Jumlah Mata Kuliah : ");
+         sam2.jum = in.nextInt();
          
-         //for (int i=0 ; i<sam2.jum ; i++){
-         //    System.out.print("Mata Kuliah : ");
-         //    sam2.mk = in.next();
-         //    System.out.print("Kode        : ");
-         //    sam2.code = in.nextInt();
-         //}
-         //sam2.aturMataKuliah(sam2.mk, sam2.code);
+         for (int i=0 ; i<sam2.jum ; i++){
+             System.out.print("Mata Kuliah : ");
+             sam2.mk = in.next();
+             System.out.print("Kode        : ");
+             sam2.code = in.nextInt();
+         }
+         sam2.aturMataKuliah(sam2.mk, sam2.code);
          
-         //System.out.print("IP       : ");
-         //sam2.ip = in.nextFloat();
-         //System.out.print("SKS      : ");
-        // sam2.sks = in.nextInt();
-         //sam2.aturIP(sam2.ip,sam2.sks);
+         System.out.print("IP       : ");
+         sam2.ip = in.nextFloat();
+         System.out.print("SKS      : ");
+         sam2.sks = in.nextInt();
+         sam2.aturIP(sam2.ip,sam2.sks);
          
          //orang ketiga
          siam2 sam3 = new siam2();
@@ -72,9 +71,9 @@ public class siam1 {
          System.out.print("Nama Mahasiswa : ");
          sam3.nama = in.nextLine();
          System.out.print("NIM : ");
-         sam3.nim = in.nextLine();
+         sam3.nim = in.nextInt();
          System.out.print("Jurusan : ");
-         sam3.jur = in.nextLine();
+         sam3.jur = in.next();
          sam3.aturData(sam3.nama, sam3.nim, sam3.jur);
          
          System.out.println("===============================");
@@ -87,7 +86,7 @@ public class siam1 {
              System.out.print("Kode        : ");
              sam3.code = in.nextInt();
          }
-         sam3.aturMataKuliah(sam3.mk, sam3.code);
+         sam.aturMataKuliah(sam3.mk, sam3.code);
          
          System.out.print("IP       : ");
          sam3.ip = in.nextFloat();
